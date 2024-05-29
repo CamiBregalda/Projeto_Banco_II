4122421d1202660aca6acee8fbecd437657b4980
Guilherme Lago <gui.lago018@gmail.com>
2024-05-29 20:03:34 -0300

    correção e adição de decimais às tabelas

--- sql/Criando tabelas.sql
+++ sql/Criando tabelas.sql
@@ -6,18 +6,39 @@ DROP TABLE IF EXISTS tb_funcionarios;
 
 
 Create table tb_fornecedores(
-	for_codigo BIGINT;
-	for_descricao VARCHAR(45) NOTNULL;
+	for_codigo BIGINT,
+	for_descricao VARCHAR(45) NOT NULL,
 	PRIMARY KEY(for_codigo)
 );
 
+Create table tb_funcionarios(
+	fun_codigo BIGINT,
+	fun_nome VARCHAR(50),
+	fun_cpf VARCHAR(50),
+	fun_senha VARCHAR(50),
+	fun_funcao VARCHAR(50),
+	PRIMARY KEY(fun_codigo)
+);
+
+Create table tb_vendas(
+	ven_codigo BIGINT,
+	ven_horario TIMESTAMP,
+	ven_valor_total DECIMAL(7,2),
+	tb_funcionarios_fun_codigo BIGINT,
+    FOREIGN KEY(ven_codigo)
+    REFERENCES tb_funcionarios(fun_codigo)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION,
+	PRIMARY KEY(ven_codigo)
+);
+
 Create table tb_produtos(
-	pro_codigo BIGINT;
-	pro_descricao VARCHAR(45);
-	pro_valor DECIMAL;
-	pro_quantidade INT;
-	tb_fornecedores_fur_codigo BIGINT;
-	FOREIGN KEY(for_codigo)
+	pro_codigo BIGINT,
+	pro_descricao VARCHAR(45),
+	pro_valor DECIMAL(7,2),
+	pro_quantidade INT,
+	tb_fornecedores_fur_codigo BIGINT,
+	FOREIGN KEY(pro_codigo)
     REFERENCES tb_fornecedores(for_codigo)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION,
@@ -25,39 +46,19 @@ Create table tb_produtos(
 );
 
 Create table tb_itens(
-	ite_codigo BIGINT;
-	ite_quantidade INT;
-	ite_valor_parcial DECIMAL;
-	tb_produtos_pro_codigo BIGINT;
-	tb_vendas_ven_codigo BIGINT;
-    FOREIGN KEY(ven_codigo)
+	ite_codigo BIGINT,
+	ite_quantidade INT,
+	ite_valor_parcial DECIMAL(7,2),
+	tb_produtos_pro_codigo BIGINT,
+	tb_vendas_ven_codigo BIGINT,
+    FOREIGN KEY(ite_codigo)
     REFERENCES tb_vendas(ven_codigo)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION,
-    FOREIGN KEY(pro_codigo)
+    FOREIGN KEY(ite_codigo)
     REFERENCES tb_produtos(pro_codigo)
       ON DELETE NO ACTION
       ON UPDATE NO ACTION,
 	PRIMARY KEY(ite_codigo)
 );
 
-Create table tb_vendas(
-	ven_codigo BIGINT;
-	ven_horario TIMESTAMP;
-	ven_valor_total DECIMAL(7,2);
-	tb_funcionarios_fun_codigo BIGINT;
-    FOREIGN KEY(fun_codigo)
-    REFERENCES tb_funcionarios(fun_codigo)
-      ON DELETE NO ACTION
-      ON UPDATE NO ACTION,
-	PRIMARY KEY(ven_codigo)
-);
-
-Create table tb_funcionarios(
-	fun_codigo BIGINT;
-	fun_nome VARCHAR(50);
-	fun_cpf VARCHAR(50);
-	fun_senha VARCHAR(50);
-	fun_funcao VARCHAR(50);
-	PRIMARY KEY(fun_codigo)
-);
