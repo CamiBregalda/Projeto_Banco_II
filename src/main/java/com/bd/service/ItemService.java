d9820eeffdb414517fa7368e843264db177d53d6
AlmadaAlmada <viniciusalmada9@gmail.com>
2024-06-13 13:49:10 -0300

    Controller-Service

---
+++ src/main/java/com/bd/service/ItemService.java
@@ -0,0 +1,65 @@
+package com.bd.service;
+
+import com.bd.exception.BusinessException;
+import com.bd.mapper.ItemMapper;
+import com.bd.model.Item;
+import com.bd.model.request.ItemRequest;
+import com.bd.model.request.UserLoginDTO;
+import com.bd.model.response.ItemResponse;
+import com.bd.model.response.ProdutoResponse;
+import com.bd.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ItemService {
+    private final ItemRepository itemRepository;
+    private final ItemMapper itemMapper;
+
+    public ItemResponse cadastrarItem(ItemRequest itemRequest) {
+        Item item =itemMapper.postDtoToEntity(itemRequest);
+        return itemMapper.entityToResponse(itemRepository.cadastrarItem(item));
+    }
+
+    public List<ItemResponse> buscarItens(UserLoginDTO userDTO) {
+        try {
+            List<Item> itens = itemRepository.buscarItens();
+
+            List<ItemResponse> itemResponses = new ArrayList<>();
+            for (Item item : itens) {
+                itemResponses.add(itemMapper.entityToResponse(item));
+            }
+            return itemResponses;
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao buscar item: " + e.getMessage());
+        }
+    }
+
+    public ItemResponse buscarItemPeloId(Long id, UserLoginDTO userDTO) {
+        try {
+            Item item = itemRepository.buscarItemPeloId(id);
+            return itemMapper.entityToResponse(item);
+        } catch (Exception e) {
+            throw new BusinessException("Erro ao buscar item: " + e.getMessage());
+        }
+    }
+
+    public ItemResponse atualizarItem(Long id, ItemRequest itemRequest, UserLoginDTO userDTO) {
+        Item item = itemMapper.postDtoToEntity(itemRequest);
+        itemRepository.atualizarItem(id, item);
+
+        return itemMapper.entityToResponse(item);
+    }
+
+    public boolean deletarItem(Long id, UserLoginDTO userDTO) {
+        return itemRepository.deletarItem(id);
+    }
+
+
+
+
+}
