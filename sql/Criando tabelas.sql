2796cfbd48430eef117cb494779e9cf61ddfbfdb
mrmutthy <gui.lago018@gmail.com>
2024-06-13 21:10:29 -0300

    criado função para o resto das tabelas

--- sql/Criando tabelas.sql
+++ sql/Criando tabelas.sql
@@ -67,7 +67,7 @@ Create table tb_usuario(
 	user_senha varchar(11)
 );
 
-INSERT INTO tb_funcionarios VALUES('1','Franscisco Aguiar', '081.654.759-66','12345678','gerente');
+--INSERT INTO tb_funcionarios VALUES('1','Franscisco Aguiar', '081.654.759-66','12345678','gerente');
 
 CREATE OR REPLACE FUNCTION gerar_registros_fornecedores()
 RETURNS VOID AS
@@ -149,7 +149,160 @@ LANGUAGE plpgsql;
 SELECT gerar_registros_produto();
 
 select * from tb_produtos;
- 
+
+
 
 select *from tb_funcionarios;
 
+CREATE OR REPLACE FUNCTION gerar_registros_funcionarios()
+RETURNS VOID AS
+$$
+DECLARE
+    i INTEGER := 1;
+    cpf_text TEXT;
+    nome TEXT[] := ARRAY['João', 'José', 'Antônio', 'Francisco', 'Carlos', 'Paulo', 'Lucas', 'Pedro', 'Marcos', 'Luiz', 'Gabriel', 'André', 'Bruno', 'Rafael', 'Felipe', 'Maria', 'Ana', 'Francisca', 'Adriana', 'Juliana', 'Fernanda', 'Patricia', 'Mariana', 'Vanessa', 'Amanda', 'Tatiane', 'Bianca', 'Marcela', 'Larissa', 'Paula'];
+    sobrenomes TEXT[] := ARRAY['Silva', 'Santos', 'Menezes', 'Souza', 'Pereira', 'Costa', 'Carvalho', 'Almeida', 'Ferreira', 'Rodrigues', 'Martins', 'Lima', 'Araújo', 'Fernandes', 'Oliveira', 'Rodrigues', 'Alves', 'Barbosa', 'Ribeiro', 'Gonçalves', 'Miranda', 'Cardoso', 'Castro', 'Rocha', 'Neves', 'Santos', 'Morais', 'Azevedo', 'Cunha', 'Mendes'];
+    posicaoNome INTEGER;
+    posicaoSobrenome INTEGER;
+    senha TEXT;
+    j INT;
+    funcao TEXT;
+    funcoes TEXT[] := ARRAY['vendedor', 'encarregado', 'gerente'];
+BEGIN
+    WHILE i <= 100 LOOP
+        -- Formatar o CPF para ter 11 dígitos
+        cpf_text := LPAD(CAST(1000000000 + i AS TEXT), 11, '0');
+        
+        posicaoNome := FLOOR(RANDOM() * (30) + 1);
+        posicaoSobrenome := FLOOR(RANDOM() * (30) + 1);
+        senha := '';
+        FOR j IN 1..8 LOOP
+            senha := senha || CAST(FLOOR(RANDOM() * 10) AS INT);
+        END LOOP;
+
+        funcao := funcoes[1 + FLOOR(RANDOM() * 3)];
+
+        INSERT INTO tb_funcionarios(fun_codigo, fun_nome, fun_cpf, fun_senha, fun_funcao)
+        VALUES (
+            i,
+            nome[posicaoNome] || ' ' || sobrenomes[posicaoSobrenome],
+            CONCAT(
+                SUBSTRING(cpf_text FROM 1 FOR 3), '.', 
+                SUBSTRING(cpf_text FROM 4 FOR 3), '.', 
+                SUBSTRING(cpf_text FROM 7 FOR 3), '-',
+                SUBSTRING(cpf_text FROM 10 FOR 2)
+            ),
+            senha,
+            funcao
+        );
+        i := i + 1;
+    END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
+
+select gerar_registros_funcionarios();
+
+select * from tb_funcionarios;
+
+CREATE OR REPLACE FUNCTION gerar_registros_vendas()
+RETURNS VOID AS
+$$
+DECLARE
+    i INTEGER := 1;
+    horario TIMESTAMP;
+    valor_total DECIMAL(7,2);
+    funcionario_codigo BIGINT;
+    produto_codigo BIGINT;
+BEGIN
+    WHILE i <= 1000 LOOP
+        -- Gerar um horário aleatório nos últimos 30 dias
+        horario := NOW() - INTERVAL '1 day' * FLOOR(RANDOM() * 30);
+        
+        -- Gerar um valor total aleatório entre 50 e 5000
+        valor_total := ROUND((RANDOM() * 4950 + 50)::NUMERIC, 2);
+
+        -- Selecionar um funcionário aleatório da tabela tb_funcionarios
+        SELECT fun_codigo
+        INTO funcionario_codigo
+        FROM tb_funcionarios
+        ORDER BY RANDOM()
+        LIMIT 1;
+
+        -- Selecionar um produto aleatório da tabela tb_produtos
+        SELECT pro_codigo
+        INTO produto_codigo
+        FROM tb_produtos
+        ORDER BY RANDOM()
+        LIMIT 1;
+
+        INSERT INTO tb_vendas(ven_codigo, ven_horario, ven_valor_total, tb_funcionarios_fun_codigo)
+        VALUES (
+            i,
+            horario,
+            valor_total,
+            funcionario_codigo
+        );
+
+        i := i + 1;
+    END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
+
+select gerar_registros_vendas();
+
+select * from tb_vendas;
+
+CREATE OR REPLACE FUNCTION gerar_registros_itens()
+RETURNS VOID AS
+$$
+DECLARE
+    i INTEGER := 1;
+    quantidade INTEGER;
+    valor_parcial DECIMAL(7,2);
+    produto_codigo BIGINT;
+    venda_codigo BIGINT;
+BEGIN
+    WHILE i <= 1000 LOOP
+        -- Selecionar um código de produto aleatório da tabela tb_produtos
+        SELECT pro_codigo
+        INTO produto_codigo
+        FROM tb_produtos
+        ORDER BY RANDOM()
+        LIMIT 1;
+
+        -- Selecionar um código de venda aleatório da tabela tb_vendas
+        SELECT ven_codigo
+        INTO venda_codigo
+        FROM tb_vendas
+        ORDER BY RANDOM()
+        LIMIT 1;
+
+        -- Gerar uma quantidade aleatória entre 1 e 10
+        quantidade := FLOOR(RANDOM() * 10 + 1)::INTEGER;
+
+        -- Calcular o valor parcial com base no valor do produto e quantidade
+        SELECT pro_valor * quantidade
+        INTO valor_parcial
+        FROM tb_produtos
+        WHERE pro_codigo = produto_codigo;
+
+        INSERT INTO tb_itens(ite_codigo, ite_quantidade, ite_valor_parcial, tb_produtos_pro_codigo, tb_vendas_ven_codigo)
+        VALUES (
+            i,
+            quantidade,
+            valor_parcial,
+            produto_codigo,
+            venda_codigo
+        );
+
+        i := i + 1;
+    END LOOP;
+END;
+$$
+LANGUAGE plpgsql;
+
+SELECT gerar_registros_itens();
+
+select * from tb_itens;
