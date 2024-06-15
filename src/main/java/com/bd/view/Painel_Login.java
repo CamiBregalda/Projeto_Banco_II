02302346081fcc39db81ea573957b1649e2ff746
Camila <camilazbregalda@gmail.com>
2024-06-15 17:35:12 -0300

    Correção tela inicial

--- src/main/java/com/bd/view/Painel_Login.java
+++ src/main/java/com/bd/view/Painel_Login.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JDialog.java to edit this template
- */
 package com.bd.view;
 
-/**
- *
- * @author Camil
- */
 public class Painel_Login extends javax.swing.JDialog {
 
-    /**
-     * Creates new form Painel_Login
-     */
+    String pessoa;
+    
     public Painel_Login(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         setTitle("Realizar Login");
