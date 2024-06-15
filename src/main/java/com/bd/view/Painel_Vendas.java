5c5eb5cf496fc8c8deb4a197dd1f2175f18cb8a0
LaraBMenezes <larabarbosabgdemenezes@gmal.com>
2024-06-15 17:16:08 -0300

    Correção do nome da label e da combo box

--- src/main/java/com/bd/view/Painel_Vendas.java
+++ src/main/java/com/bd/view/Painel_Vendas.java
@@ -38,8 +38,8 @@ public class Painel_Vendas extends javax.swing.JDialog {
         jLBQtdEstoque = new javax.swing.JLabel();
         jLBQtdEstoqueMostrar = new javax.swing.JLabel();
         jBTNComprar = new javax.swing.JButton();
-        jCBFuncionarios = new javax.swing.JComboBox<>();
-        jLabel1 = new javax.swing.JLabel();
+        jCBListaFuncionarios = new javax.swing.JComboBox<>();
+        jLBListaFuncionarios = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
 
@@ -63,10 +63,10 @@ public class Painel_Vendas extends javax.swing.JDialog {
 
         jBTNComprar.setText("Comprar");
 
-        jCBFuncionarios.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        jCBListaFuncionarios.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
 
-        jLabel1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jLabel1.setText("Funcionário responsável:");
+        jLBListaFuncionarios.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
+        jLBListaFuncionarios.setText("Funcionário responsável:");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -87,16 +87,14 @@ public class Painel_Vendas extends javax.swing.JDialog {
                                 .addGap(112, 112, 112)
                                 .addComponent(jLBValorTotalMostrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                        .addComponent(jCBFuncionarios, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addComponent(jLBPreco, javax.swing.GroupLayout.PREFERRED_SIZE, 50, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(jLBQtdEstoque)
-                            .addComponent(jLBQuantidade))
+                            .addComponent(jLBQuantidade)
+                            .addComponent(jLBListaFuncionarios))
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jCBListaFuncionarios, 0, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jSPNQuantidade)
                             .addComponent(jLBQtdEstoqueMostrar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jLBPrecoMostrar, javax.swing.GroupLayout.DEFAULT_SIZE, 141, Short.MAX_VALUE))))
@@ -109,8 +107,8 @@ public class Painel_Vendas extends javax.swing.JDialog {
                 .addComponent(jLBNomeProduto)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jCBFuncionarios, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jLabel1))
+                    .addComponent(jCBListaFuncionarios, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLBListaFuncionarios))
                 .addGap(27, 27, 27)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLBPreco)
@@ -190,7 +188,8 @@ public class Painel_Vendas extends javax.swing.JDialog {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jBTNComprar;
-    private javax.swing.JComboBox<String> jCBFuncionarios;
+    private javax.swing.JComboBox<String> jCBListaFuncionarios;
+    private javax.swing.JLabel jLBListaFuncionarios;
     private javax.swing.JLabel jLBNomeProduto;
     private javax.swing.JLabel jLBPreco;
     private javax.swing.JLabel jLBPrecoMostrar;
@@ -199,7 +198,6 @@ public class Painel_Vendas extends javax.swing.JDialog {
     private javax.swing.JLabel jLBQuantidade;
     private javax.swing.JLabel jLBValorTotal;
     private javax.swing.JLabel jLBValorTotalMostrar;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JSpinner jSPNQuantidade;
     // End of variables declaration//GEN-END:variables
